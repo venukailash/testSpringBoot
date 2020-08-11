febae6455ece697d3aad27fd2baedc5e1d9a096a
Venu Kailash <venu.kailash@wipro.com>
2020-08-12 00:36:27 +0100

    initial commit after section 6

---
+++ src/main/java/com/venukailash/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package com.venukailash.rest.webservices.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+
+	private String message;
+	
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+	
+}
